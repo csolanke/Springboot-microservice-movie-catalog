2cd2fbc3c43d296224a7f558110141847b44fa8c
Chandrakant <chandrakant@ChandrakantsMBP.lan>
2024-01-17 16:52:22 -0500

    Initial commit for movie-catalog-service

---
+++ src/main/java/com/learnwithchandrakant/moviecatologservice/MovieCatologServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learnwithchandrakant.moviecatologservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatologServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatologServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnwithchandrakant/moviecatologservice/MovieCatologServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithchandrakant.moviecatologservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatologServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
